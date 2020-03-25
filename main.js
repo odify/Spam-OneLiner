1a42e6ab7df838299749ed8442e0e6d4491a4343
odify <odify495@gmail.com>
2020-03-25 17:00:49 +0100

    V 0.0.2

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 var i = 135; setInterval(function () {
     document.write(String.fromCharCode(i)), i % 300 == 0 && document.write("<br>"), i++
 }, 2);
+var i = 135; setInterval(function () {
+    document.write(String.fromCharCode(i)), i % 300 == 0 && document.write("<br>"), i++
+}, 3);
+var i = 135; setInterval(function () {
+    document.write(String.fromCharCode(i)), i % 300 == 0 && document.write("<br>"), i++
+}, 4);
+// var i = 135; setInterval(function () {
+//     document.write(String.fromCharCode(i)), i % 300 == 0 && document.write("<br>"), i++
+// }, 2);
+// var i = 135; setInterval(function () {
+//     document.write(String.fromCharCode(i)), i % 300 == 0 && document.write("<br>"), i++
+// }, 3);
+// var i = 135; setInterval(function () {
+//     document.write(String.fromCharCode(i)), i % 300 == 0 && document.write("<br>"), i++
+// }, 4);
+// var i = 135; setInterval(function () {
+//     document.write(String.fromCharCode(i)), i % 300 == 0 && document.write("<br>"), i++
+// }, 2);
+// var i = 135; setInterval(function () {
+//     document.write(String.fromCharCode(i)), i % 300 == 0 && document.write("<br>"), i++
+// }, 3);
+// var i = 135; setInterval(function () {
+//     document.write(String.fromCharCode(i)), i % 300 == 0 && document.write("<br>"), i++
+// }, 4);
+
+
